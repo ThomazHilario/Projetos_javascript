ed85c0db0a30ef6381f78ffa8bff1988df25fd09
Thomaz Alves <thomazhilario5@gmail.com>
2023-04-23 14:16:30 -0300

    organizando código

--- Tabs/script.js
+++ Tabs/script.js
@@ -11,40 +11,47 @@ let pnotice = document.getElementById('notice')
 //Function
 
 his.addEventListener('click', function(){
-    //histori
+    //history
     his.style.background = 'white'
     his.style.transition = '0.7s'
     his.style.color = 'rgb(33, 146, 203)'
-    //outros
+    // style dos outros buttons
     notice.style.background = 'rgba(119, 119, 119, 0.452)'
     goal.style.background = 'rgba(119, 119, 119, 0.452)'
     notice.style.color = 'black'
     goal.style.color = 'black'
+    //display dos textos
     phistory.style.display = 'block'
     pgoal.style.display = 'none'
     pnotice.style.display = 'none'
 })
 goal.addEventListener('click', function(){
-    his.style.background = 'rgba(119, 119, 119, 0.452)'
-    notice.style.background = 'rgba(119, 119, 119, 0.452)'
+    //goal
     goal.style.background = 'white'
     goal.style.transition = '0.7s'
     goal.style.color = 'rgb(33, 146, 203)'
+    // style dos outros buttons
+    his.style.background = 'rgba(119, 119, 119, 0.452)'
+    notice.style.background = 'rgba(119, 119, 119, 0.452)'
     notice.style.color = 'black'
     his.style.color = 'black'
+    //display dos textos
     pgoal.style.display = 'block'
     phistory.style.display = 'none'
     pnotice.style.display = 'none'
 })
 notice.addEventListener('click', function(){
+    // notice
     notice.style.background = 'white'
-    notice.style.color = 'rgb(33, 146, 203)'
-    goal.style.color = 'black'
-    his.style.color = 'black'
     notice.style.transition = '0.7s'
+    notice.style.color = 'rgb(33, 146, 203)'
+    // style dos outros buttons
     his.style.background = 'rgba(119, 119, 119, 0.452)'
     goal.style.background = 'rgba(119, 119, 119, 0.452)'
+    goal.style.color = 'black'
+    his.style.color = 'black'
+    //display dos textos
+    pnotice.style.display = 'block'
     phistory.style.display = 'none'
     pgoal.style.display = 'none'
-    pnotice.style.display = 'block'
 })

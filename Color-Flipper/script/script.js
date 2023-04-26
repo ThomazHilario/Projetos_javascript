74e8087131d1e8a735361f3786564cf97cc986f3
Thomaz Alves <thomazhilario5@gmail.com>
2023-04-26 14:49:25 -0300

    Color Flipper reduzindo o código

--- Color-Flipper/script/script.js
+++ Color-Flipper/script/script.js
@@ -1,16 +1,6 @@
-//hexadecimal
+//Base hexadecimal
 
-//position red
-let redposition1 = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
-let redposition2 = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
-//position green
-
-let greenposition1 = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
-let greenposition2 = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
-//position Blue
-
-let blueposition1 = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
-let blueposition2 = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
+let base16 = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
 
 //Váriáveis do DOM
 
@@ -26,29 +16,12 @@ const texto = document.getElementById('texto')
 
 button.addEventListener('click',function(){
     //RGB
+
     let rgb = 'rgb' + '(' + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + "," + Math.floor(Math.random() * 255) + ')'
 
     //Hexadecimal
 
-    //red
-    let randomposition1 = Math.floor(Math.random() * redposition1.length)
-    let randomposition2 = Math.floor(Math.random() * redposition2.length)
-
-    let red = redposition1[randomposition1] + redposition2[randomposition2]
-    //green
-    let greenrandomposition1 = Math.floor(Math.random() * greenposition1.length)
-    let greenrandomposition2 = Math.floor(Math.random() * greenposition2.length)
-    
-    let green = greenposition1[greenrandomposition1] + greenposition2[greenrandomposition2]
-    //blue
-    let bluerandomposition1 = Math.floor(Math.random() * blueposition1.length)
-    let bluerandomposition2 = Math.floor(Math.random() * blueposition2.length)
-
-    let blue = blueposition1[bluerandomposition1] + blueposition2[bluerandomposition2]
-
-    //variável hexadecimal
-
-    let hex = '#' + red + green + blue
+    let hex = '#' + base16[Math.floor(Math.random() * base16.length)] + base16[Math.floor(Math.random() * base16.length)] + base16[Math.floor(Math.random() * base16.length)] + base16[Math.floor(Math.random() * base16.length)] + base16[Math.floor(Math.random() * base16.length)] + base16[Math.floor(Math.random() * base16.length)]
 
     //Sorteando o rgb e o hexadecimal
 
@@ -59,5 +32,6 @@ button.addEventListener('click',function(){
 
     //resultados
     body.style.background = rgbAndHexa[randomRgbOrHexa]
+    body.style.transition = '0.5s'
     texto.innerHTML = 'Background-color: ' + rgbAndHexa[randomRgbOrHexa]
 })

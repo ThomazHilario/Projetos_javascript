1bf084a2d22ba9b9379bb6c6d4b7277daa8feed1
Thomaz Alves <thomazhilario5@gmail.com>
2023-06-22 12:29:14 -0300

    Deixando o código mais limpo

--- Previsão-do-tempo/js/script.js
+++ Previsão-do-tempo/js/script.js
@@ -13,36 +13,60 @@ const weatherIcon = document.getElementById('weatherIcon')
 /* button event */
 
 seach.addEventListener('click',function(){
-    infor(cidade_input.value)
+    // Função para pegar a bandeira do pais
+    getFlag(cidade_input.value)
+
+    // função para pegar o nome da cidade
+    getNameCity(cidade_input.value)
+
+    // Função para pegar a temperatura da cidade
+    getTemperatureCity(cidade_input.value)
+    
+    // Função para pegar o a previsão do tempo da cidade
+    getWeatherIcon(cidade_input.value)
 })
 
 /* api */
 
-function Api(city){
+function infoApi(city){
     return fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=89b2d3b26f0d56eefdbe3bff82b60332&lang=pt_br`)
         .then(Response => Response.json())
         .catch(erro => console.log(erro))
 }
 
-async function infor(city){
+async function getFlag(city){
 
-    const api = await Api(city)
+    const apiInfo = await infoApi(city)
 
-    /* nome do pais */
+    /* bandeira do pais */
     bandeira.style.display = 'block'
-    bandeira.src = `https://flagsapi.com/${api.sys.country}/flat/64.png`
+    bandeira.src = `https://flagsapi.com/${apiInfo.sys.country}/flat/64.png`
 
-    /* nome da cidade */
+}
 
-    cidade.textContent = `Cidade: ${api.name}`
-    
-    /* temperatura */
+async function getNameCity(city){
+    const apiInfo = await infoApi(city)
 
-    const temperatura = Math.floor(api.main.temp)
-    temp.textContent = `${temperatura}°`
+     /* nome da cidade */
+     cidade.textContent = `Cidade: ${apiInfo.name}`
+}
+
+async function getTemperatureCity(city){
+
+    const apiInfo = await infoApi(city)
+
+    /* Temperatura da cidade */
+    const temperatura = Math.floor(apiInfo.main.temp)
+    temp.innerHTML = `${temperatura}°`
+}
+
+async function getWeatherIcon(city){
+
+    const apiInfo = await infoApi(city)
 
     /* weather icon e description */
     weather_layout.style.display = 'flex'
-    weather_descrition.textContent = api.weather[0].description
-    weatherIcon.src = `https://openweathermap.org/img/wn/${api.weather[0].icon}.png`
+    weather_descrition.innerHTML = apiInfo.weather[0].description
+    weatherIcon.src = `https://openweathermap.org/img/wn/${apiInfo.weather[0].icon}.png`
+
 }
